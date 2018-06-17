db85227f7ba0fbc69c4fe071c3a6e139279cd542
Zack Rowden <zacharyrowden89@gmail.com>
2018-06-17 03:00:09 +0200

    Add faces and banding scripts to each.

---
+++ visualizer/Assets/_Scripts/ParamCube.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParamCube : MonoBehaviour {
+	public int _band;
+	public float _startScale, _scaleMultiplier;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform the size based on audio parameters and public attributes defined above
+		transform.localScale = new Vector3 (transform.localScale.x, (AudioPeer._freqBand[_band] * _scaleMultiplier) * _startScale, transform.localScale.z);
+	}
+}
