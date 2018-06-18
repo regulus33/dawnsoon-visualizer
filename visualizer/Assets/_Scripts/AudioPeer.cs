d1d54863b6bcdb697ee7aa1c25dbf6fd7b9fcbd5
Zack Rowden <zacharyrowden89@gmail.com>
2018-06-18 22:53:14 +0200

    Add post processing scripts and buffer for frequency mapping.

--- visualizer/Assets/_Scripts/AudioPeer.cs
+++ visualizer/Assets/_Scripts/AudioPeer.cs
@@ -9,21 +9,37 @@ public class AudioPeer : MonoBehaviour {
 	AudioSource _audioSource;
 	public static float[] _samples = new float[512];
 	public static float[] _freqBand = new float[8];
-
+	public static float[] _bandBuffer = new float[8];
+	float[] _bufferDecrease = new float[8];
+	 
 	// Use this for initialization
 	void Start () {
 		_audioSource = GetComponent<AudioSource> ();	
 	}
-	
+		
 	// Update is called once per frame
 	void Update () {
 		GetSpectrumAudioSource ();
 		MakeFrequencyBands ();
+		BandBuffer ();
 	}
 
 	void GetSpectrumAudioSource() {
 												//Blackman is an algo for efficient iteration?
 		_audioSource.GetSpectrumData (_samples, 0, FFTWindow.Blackman); 
+	}
+
+	void BandBuffer() {
+		for (int g = 0; g < 8; ++g) {
+			if (_freqBand [g] > _bandBuffer [g]) {
+				_bandBuffer [g] = _freqBand [g];
+				_bufferDecrease [g] = 0.005f;
+			}
+			if ((_freqBand[g]) < _bandBuffer[g]) {
+				_bandBuffer[g] -= _bandBuffer[g];
+				_bufferDecrease[g] *= 1.2f;
+			}
+		}
 	}
 		
 	//we have a ton of frequency bands from 2 - 512, the math below allows us to escalate through these
--- visualizer/Assets/_Scripts/ParamCube.cs
+++ visualizer/Assets/_Scripts/ParamCube.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ParamCube : MonoBehaviour {
 	public int _band;
 	public float _startScale, _scaleMultiplier;
+	public bool _useBuffer;
 
 	// Use this for initialization
 	void Start () {
@@ -14,6 +15,15 @@ public class ParamCube : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		//transform the size based on audio parameters and public attributes defined above
+		if (_useBuffer) {
+			transform.localScale = new Vector3 (transform.localScale.x, (AudioPeer._bandBuffer [_band] * _scaleMultiplier) + _startScale, transform.localScale.z);
+		}
+
+		if (!_useBuffer) {
+			transform.localScale = new Vector3 (transform.localScale.x, (AudioPeer._freqBand [_band] * _scaleMultiplier) + _startScale, transform.localScale.z);
+		}
+
+
 		transform.localScale = new Vector3 (transform.localScale.x, (AudioPeer._freqBand[_band] * _scaleMultiplier) * _startScale, transform.localScale.z);
 	}
 }
